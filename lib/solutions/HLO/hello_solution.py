cf3b46adb2aa11a601b25966654aa137f5b963c8
sbx_user1051 <sbx_user1051@169.254.118.149>
2019-11-14 14:29:00 +0000

    Thu Nov 14 14:29:00 UTC 2019

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,10 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    """
+    Says hello world
+    param[0] = a String. Ignore for now.
+    @return = a String containing a message
+    """
+    return 'Hello'
+
