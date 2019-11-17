65e716e1f142fa83ef75bc7ce02905ee6e429507
sbx_user1051 <sbx_user1051@169.254.50.69>
2019-11-17 19:41:35 +0000

    Sun Nov 17 19:41:35 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,22 +6,24 @@ get_free_offers = {'E':[(2,(1,'B'))]} # 2E get one B free
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    value_counts = get_value_counts(skus)
 
     total_price = 0
     distinct_skus = list(set(skus))
+    # Check for invalid input
     for sku in distinct_skus:
         if sku not in prices.keys():
             return -1
 
+    # Calculate the shopping cart
     cart = get_value_counts(skus)
     # Remove free items from cart
     for sku in get_free_offers.keys():
         cart_reduced = remove_free_items_from_cart(cart, sku)
 
+    # Calculate the prices
     for sku in cart_reduced.keys():
         so_applied=False
-        number_of_occurrencies = value_counts[sku]
+        number_of_occurrencies = cart_reduced[sku]
         if sku in special_offers.keys():
             total_price += apply_special_offers(number_of_occurrencies, sku)
             so_applied = True
@@ -53,9 +55,10 @@ def apply_special_offers(value, sku):
 def remove_free_items_from_cart(cart, sku):
     quotient, reminder = divmod(cart[sku], get_free_offers[sku][0][0])
     item_to_remove = get_free_offers[sku][0][1][1]
-    cart[item_to_remove]-= quotient
-    if cart[item_to_remove] < 0:
-        cart[item_to_remove] = 0
+    if item_to_remove in cart.keys():
+        cart[item_to_remove]-= quotient
+        if cart[item_to_remove] < 0:
+            cart[item_to_remove] = 0
     return cart
 
 
@@ -64,9 +67,9 @@ if __name__ == '__main__':
     assert(checkout('ABCDE')==155)
     assert(checkout('EEB')==80)
     assert(checkout('EE')==80)
-    print(checkout('EEEEBB'))
     assert(checkout('EEEEBB')==160)
     assert(checkout('BEBEEE')==160)
+
 
 
 
