98b14e44cb8ab42bd4fc2c3b8b5355672335e8a9
sbx_user1051 <sbx_user1051@169.254.50.69>
2019-11-17 17:28:56 +0000

    Sun Nov 17 17:28:56 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,10 +4,11 @@ special_offers = {'A': (3,130), 'B':(2,45)}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skus_list = list(skus)
-    for sku in skus_list:
+    distinct_skus = list(set(skus))
+    for sku in distinct_skus:
         print(sku)
 
 if __name__ == '__main__':
-    checkout('ABC')
+    checkout('AABC')
+
 
