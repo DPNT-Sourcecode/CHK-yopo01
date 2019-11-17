3fd1a51b1caa5d70b8eba0d66384f692768b5651
sbx_user1051 <sbx_user1051@169.254.50.69>
2019-11-17 18:54:50 +0000

    Sun Nov 17 18:54:50 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -43,7 +43,10 @@ def apply_get_free_offers( value, sku):
     return reminder*prices[get_free_offers[sku][0][1][1]]
 
 if __name__ == '__main__':
-    print(checkout('AAAAAAAA'))
+    assert(checkout('E')==40)
+    assert(checkout('ABCDE')==155)
+    assert(checkout('EEB')==80)
+
 
 
 
