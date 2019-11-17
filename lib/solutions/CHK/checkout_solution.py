33511c2a32e558a2c9a4634d17ac23bbe83228a6
sbx_user1051 <sbx_user1051@169.254.50.69>
2019-11-17 17:40:56 +0000

    Sun Nov 17 17:40:56 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,9 +4,18 @@ special_offers = {'A': (3,130), 'B':(2,45)}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    total_price = 0
     distinct_skus = list(set(skus))
     for sku in distinct_skus:
-        print(sku)
+        number_of_occurrencies = skus.count(sku)
+        if sku in special_offers.keys():
+            number_of_so = number_of_occurrencies // special_offers[sku][0]
+            number_of_single_items = number_of_occurrencies % special_offers[sku][0]
+            total_price += number_of_single_items*prices[sku] +  number_of_so*special_offers[sku][1]
+        else:
+            total_price += number_of_occurrencies * prices[sku]
+    return total_price
 
 if __name__ == '__main__':
-    checkout('AABC')
+    print(checkout('AAAC'))
+
