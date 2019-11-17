7ad4bc9c2c60e2162f63d1a7ab151ba07c055961
sbx_user1051 <sbx_user1051@169.254.50.69>
2019-11-17 17:50:31 +0000

    Sun Nov 17 17:50:31 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,6 @@
 
-prices = {'A':50, 'B':30, 'C':20, 'D':15}
-special_offers = {'A': (3,130), 'B':(2,45)}
+prices = {'A':50, 'B':30, 'C':20, 'D':15, 'E': 40}
+special_offers = {'A': [(3,130),(5,200)], 'B':[(2,45)]}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
@@ -12,14 +12,15 @@ def checkout(skus):
             return -1
         number_of_occurrencies = skus.count(sku)
         if sku in special_offers.keys():
-            number_of_so = number_of_occurrencies // special_offers[sku][0]
-            number_of_single_items = number_of_occurrencies % special_offers[sku][0]
-            total_price += number_of_single_items*prices[sku] +  number_of_so*special_offers[sku][1]
+            number_of_so = number_of_occurrencies // special_offers[sku][0][0]
+            number_of_single_items = number_of_occurrencies % special_offers[sku][0][0]
+            total_price += number_of_single_items*prices[sku] + number_of_so*special_offers[sku][0][1]
         else:
             total_price += number_of_occurrencies * prices[sku]
     return total_price
 
 if __name__ == '__main__':
-    print(checkout('AAAC'))
+    print(checkout('TAAACT'))
+
 
 
